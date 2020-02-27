a96d042b1b00a966c3d4cb0cac3ee34dcf0332c9
Pieter Brandsen <pieterbrandsen2@gmail.com>
2020-02-27 13:27:34 +0100

    Special Pythagorean triplet
    Problem 9 Created

---
+++ Project Euler/Problem 9/Special Pythagorean triplet/Special Pythagorean triplet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Special_Pythagorean_triplet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
