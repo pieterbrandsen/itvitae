4a74d25d2a42754c82bfbfae4c4d9b2632ab94d9
Pieter Brandsen <pieterbrandsen2@gmail.com>
2020-02-27 17:45:11 +0100

    Summation of primes
    Problem 10 Created And Finished

---
+++ Project Euler/Problem 10/Summation of primes/Summation of primes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Summation_of_primes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int IsPrime(int number)
+            {
+                int n = number, m = n / 2, flag = 0;
+
+                for (int i = 2; i <= m; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        flag = 1;
+                        return 0;
+                    }
+                }
+                if (flag == 0)
+                {
+                    return 1;
+                }
+                return 0;
+            }
+
+            long finalNumber = 0;
+
+            for (int i = 2; i <= 2000000; i++)
+            {
+                if (IsPrime(i) == 1)
+                {
+                    finalNumber = finalNumber + i;
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("The sum of the primenumbers under 2 Million is: " + finalNumber);
+            Console.ReadKey();
+        }
+    }
+}
