444f59944f5d29450394763e9e2effa8b256e22d
Pieter Brandsen <pieterbrandsen2@gmail.com>
2020-02-27 17:00:01 +0100

    Special Pythagorean triplet
    Problem 9 Finished

--- Project Euler/Problem 9/Special Pythagorean triplet/Special Pythagorean triplet/Program.cs
+++ Project Euler/Problem 9/Special Pythagorean triplet/Special Pythagorean triplet/Program.cs
@@ -6,7 +6,21 @@ namespace Special_Pythagorean_triplet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int finalNumber = 1000;
+            int abc = 0;
+            for (int i = finalNumber / 3; i < finalNumber; i++)
+            {
+                double testNumber = i * i - finalNumber * finalNumber + 2 * finalNumber * i;
+                int first2 = (int)Math.Sqrt(testNumber);
+                if (first2 * first2 == testNumber)
+                {
+                    int b = (finalNumber - i + first2) / 2;
+                    int a = finalNumber - b - i;
+                    abc = a * b * i; break;
+                }
+            }
+            Console.WriteLine(abc);
+            Console.ReadKey();
         }
     }
 }
