81c1f6cb410a30395e320623364f2046691bff12
Pieter Brandsen <pieterbrandsen2@gmail.com>
2020-02-27 17:46:04 +0100

    Largest product in a grid
    Problem 11 Created

---
+++ Project Euler/Problem 11/Largest product in a grid/Largest product in a grid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Largest_product_in_a_grid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
