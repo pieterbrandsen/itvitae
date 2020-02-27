e2e59f72bb97cc21442805c7c425bbee16e5816d
Pieter Brandsen <pieterbrandsen2@gmail.com>
2020-02-27 12:49:09 +0100

    10001st prime
    Problem 7 Finished

--- Project Euler/Problem 7/10001st prime/10001st prime/Program.cs
+++ Project Euler/Problem 7/10001st prime/10001st prime/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _10001st_prime
 {
@@ -6,7 +7,45 @@ namespace _10001st_prime
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            static int IsPrime(int number)
+            {
+                int n = number, m = n / 2, flag = 0;
+
+                for (int i = 2; i <= m; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        flag = 1;
+                        return 0;
+                    }
+                }
+                if (flag == 0)
+                {
+                    return 1;
+                }
+                return 0;
+            }
+
+            int primeNumbers = 10500;
+            List<int> primeList = new List<int>();
+            
+
+
+            int i = 1;
+            while (primeNumbers > 0)
+            {
+                if (IsPrime(i) == 1)
+                {
+                    primeList.Add(i);
+                    Console.WriteLine(i);
+                    i++;
+                    primeNumbers--;
+                }
+                else i++;
+            }
+
+            int[] array = primeList.ToArray();
+            Console.WriteLine("The 10.001st prime is: " + array[10001]);
         }
     }
 }
