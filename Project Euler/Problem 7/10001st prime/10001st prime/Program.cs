a3db6047e563df56d24a46636cc669b5154f6fd0
Pieter Brandsen <pieterbrandsen2@gmail.com>
2020-02-27 12:29:47 +0100

    10001st prime
    Problem 7 Created

---
+++ Project Euler/Problem 7/10001st prime/10001st prime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10001st_prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
