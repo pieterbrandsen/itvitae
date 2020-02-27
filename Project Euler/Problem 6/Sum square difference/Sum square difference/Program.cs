09ab8be49438a3a6eeabf4a3321a89b07f12780a
Pieter Brandsen <pieterbrandsen2@gmail.com>
2020-02-27 12:27:26 +0100

    Sum square difference
    Project Euler 6

---
+++ Project Euler/Problem 6/Sum square difference/Sum square difference/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sum_square_difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter begin number for the sum square difference");
+            int begin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter end number for the sum square difference");
+            int end = Convert.ToInt32(Console.ReadLine());
+
+            int number1 = 0;
+            int number2 = 0;
+            for (int i = begin; i <= end; i++)
+            {
+                number1 = number1 + i;
+                number2 = number2 + i * i;
+                Console.WriteLine(i);
+            }
+            number1 = number1 * number1;
+            int finalNumber = number1 - number2;
+
+            Console.WriteLine("The difference between the sum of the squares of the first ten natural numbers and the square of the sum is: " + finalNumber);
+
+        }
+    }
+}
